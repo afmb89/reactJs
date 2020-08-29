db2d8a2e616f5cdc8a5b6eb1220d09eaf167786d
afmb89 <afmb_231089@hotmail.com>
2020-08-29 01:17:19 +0000

    listo unidad_1 optativo

--- unidad_1/script.js
+++ unidad_1/script.js
@@ -1,3 +1,9 @@
+function elegir(){
+    var tipoSeguro = document.getElementById("tipo-seguro");
+    var tipoSeguroValor = tipoSeguro.options[tipoSeguro.selectedIndex].value;
+    document.getElementById("tipo-seguro-valor").value = tipoSeguroValor;
+}
+
 function saludar(){
     alert("Muchas gracias por confiar en nostros!!!");
 }
