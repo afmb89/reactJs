9c536c55e7b567433524ee60dd254b721f84573d
afmb89 <afmb_231089@hotmail.com>
2020-08-31 13:08:57 +0000

    v3 unidad_2_optativo

---
+++ unidad_2_optativo/script.js
@@ -0,0 +1,11 @@
+var operador1 =  document.getElementById("operador1");
+var operador2 =  document.getElementById("operador2");
+var resultado =  document.getElementById("resultado");
+
+function borrar(){
+    
+    operador1.value="";
+    operador2.value="";
+    resultado.value="";
+
+}
